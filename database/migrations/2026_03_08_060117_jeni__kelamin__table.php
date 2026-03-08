0418b48a8a744a4071c79173320e9e6c5069ab1c
Raani <safrianibelajarmultimedia@gmail.com>
2026-03-08 13:01:54 +0700

    Tugas jaya

--- database/migrations/2026_03_08_060117_jeni__kelamin__table.php
+++ database/migrations/2026_03_08_060117_jeni__kelamin__table.php
@@ -11,11 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('desa', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama_desa');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -23,6 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('desa');
+        //
     }
 };
